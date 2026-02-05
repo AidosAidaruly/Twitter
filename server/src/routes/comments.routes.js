49a2e5eb545f7a446695e609cb5f8dd2dab05bf8
AidosAidaruly <aidosfiyoy677@gmail.com>
2026-02-06 00:56:00 +0600

    Delete server directory

--- server/src/routes/comments.routes.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const { requireAuth } = require("../middleware/auth");
-const { deleteComment } = require("../controllers/comments.controller");
-
-router.delete("/:id", requireAuth, deleteComment);
-
-module.exports = router;
